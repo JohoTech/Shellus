1eeeae21c91b990f9ad51151547401ee9a2da872
Thrall <thrall@kalimdor.wow>
2016-07-10 19:52:47 -0300

    Commit test

--- clients/squid/squid_helper.py
+++ clients/squid/squid_helper.py
@@ -2,10 +2,17 @@ from squidhelper import squid_module
 import sys
 import syslog
 import re
+import yaml
 
-redis = squid_module.redis_con("172.17.0.2")
+with open('./squid_helper.yml','r') as conf_file:
+    conf = yaml.load(conf_file)
+
+host = conf['redis']['server']
+port = conf['redis']['port']
+password = conf['redis']['password']
+
+redis = squid_module.redis_con(host,port,password)
 
 while True:
     ip_to_check = input()
-    #print(squid_module.check_ip_rule(redis, sys.argv[0]))
     print(squid_module.check_ip_rule(redis, ip_to_check))
--- clients/squid/squidhelper/squid_module.py
+++ clients/squid/squidhelper/squid_module.py
@@ -1,14 +1,6 @@
 import os
 import redis
 import rq
-import yaml
-
-with open('../squid_helper.yml','r') as conf_file:
-    conf = yaml.load(conf_file)
-
-host = conf['redis']['server']
-port = conf['redis']['port']
-password = conf['redis']['password']
 
 def redis_con(host,port,password):
     con = redis.Redis(host=host,port=port,password=password)
